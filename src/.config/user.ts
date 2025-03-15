ddc3c31359787f12eaf8392fa2873f594543ab95
nkzd <aljosa.vukotic@gmail.com>
2025-03-15 20:10:50 +0100

    Config changes

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -4,4 +4,47 @@ export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   // site: { title: "講評世界" },
   // seo: { twitter: "@moeyua13" },
+  site: {
+    title: 'Alyosha\'s Blog',
+    author: 'Alyosha.net',
+    subtitle: '',
+    description: 'Personal Blog',
+    website: 'https://alyosha.net',
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/nkzd',
+      },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+      {
+        name: 'twitter',
+        href: 'https://twitter.com/AlyoshaDev',
+      },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+  },
+  appearance: {
+    locale: 'en-us',
+    theme: 'light',
+  },
+  seo: {
+    twitter: '@AlyoshaDev',
+  },
+
 }
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,6 +1,6 @@
 ---
-title: 关于
-pubDate: 2024-12-06
+title: About me
+pubDate: 2025-03-14
 ---
 
-此主题系 Hexo 主题 <a href="https://github.com/sumimakito/hexo-theme-typography">活版印字</a> 在 Astro 平台上的移植版本
+Software engineer, looking to improve his writing.
